a943edc608259d59d49f2002b689759ed19b9c00
Christian Bøgelund <s171243@student.dtu.dk>
2019-10-23 20:16:49 +0800

    Added (ugly) layout

--- client/Client.java
+++ client/Client.java
@@ -8,9 +8,13 @@ import java.util.Scanner;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Client extends Application {
@@ -74,20 +78,68 @@ public class Client extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
+        BorderPane root = new BorderPane();
         primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        //bottom
+        Button btn = new Button("Submit");
+        btn.setPrefWidth(100);
+        btn.setPrefHeight(100);
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        TextField bottom = new TextField();
+        bottom.setPrefWidth(400);
+        bottom.setPrefHeight(100);
+        bottom.setPromptText("This is a test");
+
+        HBox bottomFrame = new HBox(2);
+        bottomFrame.getChildren().addAll(bottom, btn);
+        root.setBottom(bottomFrame);
+
+        //right
+        Label label = new Label("Online users");
+        label.setStyle("-fx-font-weight: bold");
+        label.setPadding(new Insets(10, 10, 10, 10));
+
+        Label label2 = new Label("User 1");
+        label.setPadding(new Insets(10, 10, 10, 10));
+
+        Label label3 = new Label("User 2");
+        label.setPadding(new Insets(10, 10, 10, 10));
+
+        Label label4 = new Label("User 3");
+        label.setPadding(new Insets(10, 10, 10, 10));
+
+        VBox vbox = new VBox(10);
+        vbox.getChildren().addAll(label, label2, label3, label4);
+        vbox.setPadding(new Insets(20, 10, 20, 20));
+        vbox.setBackground(new Background(new BackgroundFill(Color.rgb(255, 255, 255), CornerRadii.EMPTY, Insets.EMPTY)));
+
+        root.setRight(vbox);
+
+        // center
+        VBox center = new VBox(10);
+
+        Label label5 = new Label("Chat");
+        label5.setStyle("-fx-font-weight: bold");
+        label.setPadding(new Insets(10, 10, 10, 10));
+
+        Label label6 = new Label("Message");
+        label.setPadding(new Insets(10, 10, 10, 10));
+
+        Label label7 = new Label("Message");
+        label.setPadding(new Insets(10, 10, 10, 10));
+
+        Label label8 = new Label("Message");
+        label.setPadding(new Insets(10, 10, 10, 10));
+
+        VBox centerBox = new VBox(10);
+        centerBox.getChildren().addAll(label5, label6, label7, label8);
+        centerBox.setPadding(new Insets(50, 10, 50, 20));
+
+        root.setCenter(centerBox);
+
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
 }
