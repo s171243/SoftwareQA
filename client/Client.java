7be8b1c3936048bd8036de97746e5a13b8eeea88
Christian Bøgelund <s171243@student.dtu.dk>
2019-10-23 15:09:20 +0800

    Finally made client work 100%

--- client/Client.java
+++ client/Client.java
@@ -1,13 +1,9 @@
 package client;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client
 {
@@ -20,27 +16,43 @@ public class Client
         {
             String host = "localhost";
             int port = 9000;
+            Scanner myScanner = new Scanner(System.in);
             InetAddress address = InetAddress.getByName(host);
             socket = new Socket(address, port);
 
             //Send the message to the server
             OutputStream os = socket.getOutputStream();
             OutputStreamWriter osw = new OutputStreamWriter(os);
-            BufferedWriter bw = new BufferedWriter(osw);
+            PrintWriter bw = new PrintWriter(osw);
 
             String number = "LIST";
 
-            String sendMessage = number + "\n";
-            bw.write(sendMessage);
-            bw.flush();
-            System.out.println("Message sent to the server : " + sendMessage);
+            //String sendMessage = number + "\n";
+            //bw.write(sendMessage);
+            //bw.flush();
+            //System.out.println("Message sent to the server : " + sendMessage);
 
             //Get the return message from the server
             InputStream is = socket.getInputStream();
             InputStreamReader isr = new InputStreamReader(is);
             BufferedReader br = new BufferedReader(isr);
-            String message = br.readLine();
-            System.out.println("Message received from the server : " +message);
+            //String message = br.readLine();
+            //System.out.println("Message received from the server : " + message);
+
+            while(true){
+                String msg = myScanner.nextLine();
+
+                if(msg.equals("QUIT")){
+                    socket.close();
+                    break;
+                }
+                //String msg =  + "\n";
+                bw.println(msg + "\n");
+                System.out.println("Message sent to the server : " + msg);
+                bw.flush();
+                String response = br.readLine();
+                System.out.println("Message received from the server : " + response);
+            }
         }
         catch (Exception exception)
         {
